e4f6813671372568bdcf06a66d84be50a767a924
Son Pham <phamhoangson2611@gmail.com>
2025-11-29 10:57:09 +0700

    init db schema and auth flow

---
+++ lib/supabase/schema.ts
@@ -0,0 +1,489 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[];
+
+export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "13.0.5";
+  };
+  public: {
+    Tables: {
+      forecasts: {
+        Row: {
+          flow_rate: number | null;
+          forecast_date: string;
+          id: number;
+          model_id: number | null;
+          rainfall: number | null;
+          station_id: number;
+          target_date: string;
+          water_level: number | null;
+        };
+        Insert: {
+          flow_rate?: number | null;
+          forecast_date: string;
+          id?: number;
+          model_id?: number | null;
+          rainfall?: number | null;
+          station_id: number;
+          target_date: string;
+          water_level?: number | null;
+        };
+        Update: {
+          flow_rate?: number | null;
+          forecast_date?: string;
+          id?: number;
+          model_id?: number | null;
+          rainfall?: number | null;
+          station_id?: number;
+          target_date?: string;
+          water_level?: number | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "forecasts_model_id_fkey";
+            columns: ["model_id"];
+            isOneToOne: false;
+            referencedRelation: "model_configs";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "forecasts_station_id_fkey";
+            columns: ["station_id"];
+            isOneToOne: false;
+            referencedRelation: "stations";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      measurements: {
+        Row: {
+          created_at: string;
+          fetched_at: string | null;
+          flow_rate: number | null;
+          id: number;
+          measured_at: string;
+          rainfall: number | null;
+          rainfall_12h: number | null;
+          rainfall_1h: number | null;
+          rainfall_24h: number | null;
+          rainfall_6h: number | null;
+          rainfall_7to7: number | null;
+          station_id: number;
+          water_level: number | null;
+        };
+        Insert: {
+          created_at?: string;
+          fetched_at?: string | null;
+          flow_rate?: number | null;
+          id?: number;
+          measured_at: string;
+          rainfall?: number | null;
+          rainfall_12h?: number | null;
+          rainfall_1h?: number | null;
+          rainfall_24h?: number | null;
+          rainfall_6h?: number | null;
+          rainfall_7to7?: number | null;
+          station_id: number;
+          water_level?: number | null;
+        };
+        Update: {
+          created_at?: string;
+          fetched_at?: string | null;
+          flow_rate?: number | null;
+          id?: number;
+          measured_at?: string;
+          rainfall?: number | null;
+          rainfall_12h?: number | null;
+          rainfall_1h?: number | null;
+          rainfall_24h?: number | null;
+          rainfall_6h?: number | null;
+          rainfall_7to7?: number | null;
+          station_id?: number;
+          water_level?: number | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "measurements_station_id_fkey";
+            columns: ["station_id"];
+            isOneToOne: false;
+            referencedRelation: "stations";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      model_configs: {
+        Row: {
+          algorithm: string;
+          created_at: string;
+          created_by: string | null;
+          id: number;
+          is_stable: boolean | null;
+          name: string;
+          parameters: Json | null;
+        };
+        Insert: {
+          algorithm: string;
+          created_at?: string;
+          created_by?: string | null;
+          id?: number;
+          is_stable?: boolean | null;
+          name: string;
+          parameters?: Json | null;
+        };
+        Update: {
+          algorithm?: string;
+          created_at?: string;
+          created_by?: string | null;
+          id?: number;
+          is_stable?: boolean | null;
+          name?: string;
+          parameters?: Json | null;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "model_configs_created_by_fkey";
+            columns: ["created_by"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      role_permissions: {
+        Row: {
+          id: number;
+          permission: Database["public"]["Enums"]["app_permission"];
+          role: Database["public"]["Enums"]["app_role"];
+        };
+        Insert: {
+          id?: number;
+          permission: Database["public"]["Enums"]["app_permission"];
+          role: Database["public"]["Enums"]["app_role"];
+        };
+        Update: {
+          id?: number;
+          permission?: Database["public"]["Enums"]["app_permission"];
+          role?: Database["public"]["Enums"]["app_role"];
+        };
+        Relationships: [];
+      };
+      stations: {
+        Row: {
+          alarm_level: number | null;
+          country: string | null;
+          created_at: string;
+          flood_level: number | null;
+          id: number;
+          is_deleted: boolean | null;
+          latitude: number;
+          longitude: number;
+          name: string;
+          region: string | null;
+          river: string | null;
+          station_code: string;
+        };
+        Insert: {
+          alarm_level?: number | null;
+          country?: string | null;
+          created_at?: string;
+          flood_level?: number | null;
+          id?: number;
+          is_deleted?: boolean | null;
+          latitude: number;
+          longitude: number;
+          name: string;
+          region?: string | null;
+          river?: string | null;
+          station_code: string;
+        };
+        Update: {
+          alarm_level?: number | null;
+          country?: string | null;
+          created_at?: string;
+          flood_level?: number | null;
+          id?: number;
+          is_deleted?: boolean | null;
+          latitude?: number;
+          longitude?: number;
+          name?: string;
+          region?: string | null;
+          river?: string | null;
+          station_code?: string;
+        };
+        Relationships: [];
+      };
+      user_roles: {
+        Row: {
+          id: number;
+          role: Database["public"]["Enums"]["app_role"];
+          user_id: string;
+        };
+        Insert: {
+          id?: number;
+          role: Database["public"]["Enums"]["app_role"];
+          user_id: string;
+        };
+        Update: {
+          id?: number;
+          role?: Database["public"]["Enums"]["app_role"];
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "user_roles_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      users: {
+        Row: {
+          created_at: string;
+          email: string | null;
+          full_name: string | null;
+          id: string;
+        };
+        Insert: {
+          created_at?: string;
+          email?: string | null;
+          full_name?: string | null;
+          id: string;
+        };
+        Update: {
+          created_at?: string;
+          email?: string | null;
+          full_name?: string | null;
+          id?: string;
+        };
+        Relationships: [];
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      authorize: {
+        Args: {
+          requested_permission: Database["public"]["Enums"]["app_permission"];
+          user_id: string;
+        };
+        Returns: boolean;
+      };
+    };
+    Enums: {
+      app_permission:
+        | "users.manage"
+        | "data.manage"
+        | "models.tune"
+        | "data.download";
+      app_role: "admin" | "data_scientist";
+    };
+    CompositeTypes: {
+      [_ in never]: never;
+    };
+  };
+};
+
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<
+  keyof Database,
+  "public"
+>];
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R;
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I;
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U;
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
+    | { schema: keyof DatabaseWithoutInternals },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never;
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals;
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals;
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never;
+
+export const Constants = {
+  public: {
+    Enums: {
+      app_permission: [
+        "users.manage",
+        "data.manage",
+        "models.tune",
+        "data.download",
+      ],
+      app_role: ["admin", "data_scientist"],
+    },
+  },
+} as const;
+
+// Schema: __InternalSupabase
+// Schema: public
+// Enums
+export enum AppPermission {
+  usersManage = "users.manage",
+  dataManage = "data.manage",
+  modelsTune = "models.tune",
+  dataDownload = "data.download",
+}
+
+export enum AppRole {
+  admin = "admin",
+  data_scientist = "data_scientist",
+}
+
+// Tables
+export type Forecasts = Database["public"]["Tables"]["forecasts"]["Row"];
+export type InsertForecasts =
+  Database["public"]["Tables"]["forecasts"]["Insert"];
+export type UpdateForecasts =
+  Database["public"]["Tables"]["forecasts"]["Update"];
+
+export type Measurements = Database["public"]["Tables"]["measurements"]["Row"];
+export type InsertMeasurements =
+  Database["public"]["Tables"]["measurements"]["Insert"];
+export type UpdateMeasurements =
+  Database["public"]["Tables"]["measurements"]["Update"];
+
+export type ModelConfigs = Database["public"]["Tables"]["model_configs"]["Row"];
+export type InsertModelConfigs =
+  Database["public"]["Tables"]["model_configs"]["Insert"];
+export type UpdateModelConfigs =
+  Database["public"]["Tables"]["model_configs"]["Update"];
+
+export type RolePermissions =
+  Database["public"]["Tables"]["role_permissions"]["Row"];
+export type InsertRolePermissions =
+  Database["public"]["Tables"]["role_permissions"]["Insert"];
+export type UpdateRolePermissions =
+  Database["public"]["Tables"]["role_permissions"]["Update"];
+
+export type Stations = Database["public"]["Tables"]["stations"]["Row"];
+export type InsertStations = Database["public"]["Tables"]["stations"]["Insert"];
+export type UpdateStations = Database["public"]["Tables"]["stations"]["Update"];
+
+export type UserRoles = Database["public"]["Tables"]["user_roles"]["Row"];
+export type InsertUserRoles =
+  Database["public"]["Tables"]["user_roles"]["Insert"];
+export type UpdateUserRoles =
+  Database["public"]["Tables"]["user_roles"]["Update"];
+
+export type Users = Database["public"]["Tables"]["users"]["Row"];
+export type InsertUsers = Database["public"]["Tables"]["users"]["Insert"];
+export type UpdateUsers = Database["public"]["Tables"]["users"]["Update"];
+
+// Functions
+export type ArgsAuthorize =
+  Database["public"]["Functions"]["authorize"]["Args"];
+export type ReturnTypeAuthorize =
+  Database["public"]["Functions"]["authorize"]["Returns"];
